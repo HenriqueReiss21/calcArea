8e8086c5d81e2b8eee245cad75b784c90d71cd8c
HenriqueReiss21 <85037643+HenriqueReiss21@users.noreply.github.com>
2022-10-17 14:28:59 -0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.calcarea;
+package br.com.bq.calcarearange;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,21 +15,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rangeAlturaSelectListener();
+        rangeLarguraSelectListener();
+    }
 
-        calc();
+    public void calculaArea() {
+        final EditText outputArea = findViewById(R.id.outputArea);
+        final TextView textSeekBarLargura = findViewById(R.id.textSeekBarLargura);
+        final TextView textSeekBarAltura = findViewById(R.id.textSeekBarAltura);
+        double alturaDecimal = Double.parseDouble(textSeekBarAltura.getText().toString());
+        double larguraDecimal = Double.parseDouble(textSeekBarLargura.getText().toString());
+        double areaDecimal = alturaDecimal * larguraDecimal;
+        outputArea.setText(Double.toString(areaDecimal));
 
     }
-    public void calc()
-    {
-        final Button button = (Button) findViewById(R.id.b_calc);
-        button.setOnClickListener(new View.OnClickListener(){
+
+    public void rangeLarguraSelectListener() {
+        SeekBar seekBarTest = findViewById(R.id.seekBar5);
+        final TextView textSeekBarLargura = findViewById(R.id.textSeekBarLargura);
+        seekBarTest.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v)
-            {
-                int height = Integer.parseInt(((EditText) findViewById(R.id.e_height)).getText().toString());
-                int width = Integer.parseInt(((EditText) findViewById(R.id.e_width)).getText().toString());
-                TextView area = (TextView) findViewById(R.id.area_text);
-                area.setText("Área = " + Integer.toString(height*width));
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textSeekBarLargura.setText(String.valueOf(i));
+                calculaArea();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void rangeAlturaSelectListener() {
+        SeekBar seekBarTest = findViewById(R.id.seekBar3);
+        final TextView textSeekBarAltura = findViewById(R.id.textSeekBarAltura);
+        seekBarTest.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textSeekBarAltura.setText(String.valueOf(i));
+                calculaArea();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
